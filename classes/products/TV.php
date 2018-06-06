0f3811aeee729ae42fa7d5ace6e712d02abdff8c
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-07 00:18:15 +0300

    Update TV.php

--- classes/products/TV.php
+++ classes/products/TV.php
@@ -1,11 +1,13 @@
 <?php
-namespace classes\Products;
-class TV extends ParentForType
-{
-    public $resolution = '4K';
-    public $color = 'black';
-    public function setTVResolution($resolution)
+
+    namespace classes\Products;
+
+    class TV extends ParentForType
     {
-        $this->resolution = $resolution;
+        public $resolution = '4K';
+        public $color = 'black';
+        public function setTVResolution($resolution)
+        {
+            $this->resolution = $resolution;
+        }
     }
-}
