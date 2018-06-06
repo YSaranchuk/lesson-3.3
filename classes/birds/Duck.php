b0bf9d9478fb5f5c48ac858208e090df4745310b
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-07 00:24:20 +0300

    Update Duck.php

--- classes/birds/Duck.php
+++ classes/birds/Duck.php
@@ -1,7 +1,9 @@
 <?php
-namespace classes\birds;
-class Duck extends Birds
-{
-    protected $type = 'Duck';
-    protected $sound = 'Кря';
-}
+
+    namespace classes\birds;
+
+    class Duck extends Birds
+    {
+        protected $type = 'Duck';
+        protected $sound = 'Кря';
+    }
