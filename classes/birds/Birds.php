2d7a5b2d93ce0740c81527588e379e5899003326
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-07 00:23:41 +0300

    Update Birds.php

--- classes/birds/Birds.php
+++ classes/birds/Birds.php
@@ -1,16 +1,18 @@
 <?php
-namespace classes\birds;
-abstract class Birds extends \classes\Goods implements \interfaces\BirdsInterface
-{
-    protected $rich; 
-    protected $habbitat;
-    public function __construct($rich, $habbitat)
+
+    namespace classes\birds;
+
+    abstract class Birds extends \classes\Goods implements \interfaces\BirdsInterface
     {
-        $this->rich = $rich;
-        $this->habbitet = $habbitat;
-    }
-    public function makeSound()
-    {
-        echo $this->rich . ' говорит - "' . $this->sound . '"<br>';
-    }
-}
+        protected $rich; 
+        protected $habbitat;
+        public function __construct($rich, $habbitat)
+        {
+            $this->rich = $rich;
+            $this->habbitet = $habbitat;
+        }
+        public function makeSound()
+        {
+            echo $this->rich . ' говорит - "' . $this->sound . '"<br>';
+        }
+   }
