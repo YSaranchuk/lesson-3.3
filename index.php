d19491acd8f7748ee7fb037e1caa8b6e9595d7bb
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-06 23:51:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php
-function myAutoload($className) {  
-    $pathToFile = $className . '.php';
-    $separator = array('\\', '/');
-    $pathToFile = str_replace( $separator , DIRECTORY_SEPARATOR, $pathToFile );
+    function myAutoload($className) {  
+        $pathToFile = $className . '.php';
+        $separator = array('\\', '/');
+        $pathToFile = str_replace( $separator , DIRECTORY_SEPARATOR, $pathToFile );
     if (file_exists($pathToFile)) {
         include "$pathToFile"; 
     } else {
         echo "<br><span style=\"font-size: 22px; color: red; font-style: italic; \";>Файл с классом \"$className\" не найден.</span>";
     }
-};
-spl_autoload_register('myAutoload');
-$duckEn = new \classes\birds\Scrooge('Scrooge McDuck', '');
-$duckEn->makeSound(); 
-$penParker = new \classes\products\BallpointPen('Parker Jotter', 3000);
-$penParker->setMark();
+    };
+    spl_autoload_register('myAutoload');
+    $duckEn = new \classes\birds\Scrooge('Scrooge McDuck', '');
+    $duckEn->makeSound(); 
+    $penParker = new \classes\products\BallpointPen('Parker Jotter', 3000);
+    $penParker->setMark();
 
-$penPero = new \classes\products\BallpointPen('Pero Caran', 3500, 'черная');
-$penPero->setMark();
+    $penPero = new \classes\products\BallpointPen('Pero Caran', 3500, 'черная');
+    $penPero->setMark();
 
-$Mini = new \classes\products\Car('MINICooper', 1500000);
-$Mini->setMark();
+    $Mini = new \classes\products\Car('MINICooper', 1500000);
+    $Mini->setMark();
 
-$tvLG = new \classes\products\TV('LG 43UH610V', 49000);
-$tvLG->setMark();
+    $tvLG = new \classes\products\TV('LG 43UH610V', 49000);
+    $tvLG->setMark();
 
-$tvSony = new \classes\products\TV('Sony KD-65XE9305', 46000);
-$tvSony->setMark();
+    $tvSony = new \classes\products\TV('Sony KD-65XE9305', 46000);
+    $tvSony->setMark();
 ?>
 
 <!DOCTYPE html>
@@ -41,10 +41,7 @@ $tvSony->setMark();
         h1 {
             font-style: italic;
             font-size: 22px;
-            color: green;
-            
-            
-            
+            color: green;   
         }
         body {
             max-width: 1000px;
@@ -66,32 +63,32 @@ $tvSony->setMark();
 
 <h1>История корзины</h1>
 <?php
-$Baskett = new \classes\Basket();
-$Baskett->addProduct($penParker);
-$Baskett->addProduct($penParker);
-$Baskett->addProduct($penParker);
+    $Baskett = new \classes\Basket();
+    $Baskett->addProduct($penParker);
+    $Baskett->addProduct($penParker);
+    $Baskett->addProduct($penParker);
 
-echo '<br>';
-$Baskett->addProduct($penParker);
-$Baskett->addProduct($tvLG);
-$Baskett->addProduct($tvSony);
-echo '<br>';
-$Baskett->deleteOneProduct($penParker);
-$Baskett->deleteOneProduct($tvSony);
+    echo '<br>';
+    $Baskett->addProduct($penParker);
+    $Baskett->addProduct($tvLG);
+    $Baskett->addProduct($tvSony);
+    echo '<br>';
+    $Baskett->deleteOneProduct($penParker);
+    $Baskett->deleteOneProduct($tvSony);
 
-echo '<br>';
-$Baskett->addProduct($Mini);
+    echo '<br>';
+    $Baskett->addProduct($Mini);
 ?>
 
 <h1>Вывод корзины</h1>
 <?php
-$Baskett->showAllProduct(); 
+    $Baskett->showAllProduct(); 
 ?>
 <h1>Вывод заказа</h1>
 <?php
-$Order = new \classes\Order();
-$Order->setOrder($Baskett); 
-$Order->showAllProduct(); 
+    $Order = new \classes\Order();
+    $Order->setOrder($Baskett); 
+    $Order->showAllProduct(); 
 ?>
 <br>
 <button>Подтвердить заказ</button>
