4d8dcffa083f341ce9850afd0a1dc614ee3a13be
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-07 00:54:32 +0300

    Update Car.php

--- classes/products/Car.php
+++ classes/products/Car.php
@@ -2,8 +2,7 @@
 
     namespace classes\products;
 
-    class Car extends ParentForType implements \interfaces\CarInterface
-    {
+    class Car extends ParentForType implements \interfaces\CarInterface {
         public $power = '300 лошадиных сил';
         public $type = 'Car';
         public function setCarPower($horp)
